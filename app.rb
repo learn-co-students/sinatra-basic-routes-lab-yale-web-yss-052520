93e5852132c8d9b30e66ece689a141389a5475ae
ZIming Mao <ziming.mao@yale.edu>
2020-06-13 11:43:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/hometown' do 
+        "My hometown is _"
+    end
+
+    get '/name' do 
+        "My name is __"
+    end
+
+    get '/favorite-song' do
+
+        "My favorite song is __"
+    end
 end
