e1aeb15fa55afd7095f9c5eb256678477d567b7a
davidyu7 <david.yu@yale.edu>
2020-06-15 10:26:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Dave"
+    end
+
+    get '/hometown' do
+        "My hometown is Fort Lee"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is friday"
+    end
 end
