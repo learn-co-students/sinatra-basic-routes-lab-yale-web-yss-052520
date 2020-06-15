65e234fdef006c288431689eb43e74b48d29e9dd
mlkelly <maurak87@gmail.com>
2020-06-15 16:43:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/name' do
+        "My name is Maura"
+   end
+
+   get '/hometown' do
+        "My hometown is Pittsburgh, PA!!"
+   end
+
+   get '/favorite-song' do 
+        "My favorite song is Helplessness Blues by Fleet Foxes"
+   end
+
 end
