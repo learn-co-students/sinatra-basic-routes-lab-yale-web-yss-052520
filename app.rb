c15ff85249e67077a5eb83bd9aa3f04999528ec8
s-dney <littlebirdsword@gmail.com>
2020-06-15 09:42:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sid!"
+  end
+
+  get '/hometown' do
+    "My hometown is San Carlos, California"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Gloria by Laura Branigan"
+  end
+
 end
