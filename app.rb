bf035d5744e3e1601269d0f8cb069370314ca7d9
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-14 21:50:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do 
+        "My name is _"
+    end 
+
+    get "/hometown" do 
+        "My hometown is __"
+    end 
+
+    get "/favorite-song" do 
+        "My favorite song is __"
+    end 
 end
+
