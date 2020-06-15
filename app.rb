334947822c14487104817aa708e2715858154dab
adilgondal1 <adil.gondal@yale.edu>
2020-06-15 17:15:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Adil"
+    end
+
+    get '/hometown' do
+        "My hometown is Brooklyn."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 1985."
+    end
+
 end
