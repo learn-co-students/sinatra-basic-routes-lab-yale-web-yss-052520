c169c14769dc24a7b083896451cd9f2f2f362f63
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-06-16 14:00:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Tilman"
+  end
+
+  get '/hometown' do
+    "My hometown is Aschaffenburg"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Defiler by Stromae"
+  end
 end
