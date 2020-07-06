813ca2e1d37fc7fa7ff09a874950c3259cdba3a5
arthur318 <arthurhwang318@gmail.com>
2020-07-06 01:09:33 -0700

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        erb :'name.html.erb'
+    end
+
+    get '/hometown' do
+        erb :'hometown.html.erb'
+    end
+
+    get '/favorite-song' do
+        erb :'favorite-song.html.erb'
+    end
+
 end
